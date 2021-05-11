10dffda7991e93ffd3f02e6831dd1f6af3ed7827
Chanrich Pisitjing <vjumpkung@gmail.com>
2021-05-11 13:44:07 +0700

    update messenger.js

--- components/Messenger.js
+++ components/Messenger.js
@@ -2,7 +2,7 @@ import Head from "next/head"
 const Messenger = () => {
     return (
         <div>
-            <Head><script src="https://herocartoontshirt.my.to/messenger.js" /></Head>
+            <Head><script src="/messenger.js" /></Head>
             <div id="fb-root"></div>
             <div className="fb-customerchat" attribution="biz_inbox" page_id="567182800427506"></div>
         </div>
