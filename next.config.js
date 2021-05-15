c9adb4950f870b7bc2ad5d72218a40f2cd27e132
Chanrich Pisitjing <vjumpkung@gmail.com>
2021-05-15 14:13:44 +0700

    tttt

--- next.config.js
+++ next.config.js
@@ -6,18 +6,7 @@ module.exports = {
   async headers() {
     return [
       {
-        source: '/:all*(svg|jpg|png|webp)',
-        locale: false,
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=9999999999, must-revalidate',
-          }
-        ],
-      },
-      {
-        source: '/_next/image(.*)',
-        locale: false,
+        source: '/(.*)',
         headers: [
           {
             key: 'Cache-Control',
