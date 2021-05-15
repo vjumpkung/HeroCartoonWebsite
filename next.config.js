db042686058f32327f8addae71966aa74a4860d1
Chanrich Pisitjing <vjumpkung@gmail.com>
2021-05-15 13:27:09 +0700

    f**k

--- next.config.js
+++ next.config.js
@@ -3,4 +3,18 @@ module.exports = {
   images: {
     domains: ["admin.herocartoontshirt.com"],
   },
+  async headers() {
+    return [
+      {
+        source: '/:all*(svg|jpg|png)',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=9999999999, must-revalidate',
+          }
+        ],
+      },
+    ]
+  },
 };
