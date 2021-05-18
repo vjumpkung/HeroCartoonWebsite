0b4872318adc1ffc28b373e9a6c5699e6a1ba357
Chanrich Pisitjing <vjumpkung@gmail.com>
2021-05-18 23:05:39 +0700

    oops

--- apollo-client.js
+++ apollo-client.js
@@ -1,5 +1,18 @@
 import { ApolloClient, InMemoryCache, } from "@apollo/client";
 const client = new ApolloClient({
   uri: "https://admin.herocartoontshirt.com/graphql",
+  cache: new InMemoryCache(),
+  defaultOptions: defaultOptions,
+
 });
+const defaultOptions = {
+  watchQuery: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'ignore',
+  },
+  query: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'all',
+  },
+}
 export default client;
