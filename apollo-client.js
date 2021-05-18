622661e7c60f5696c56223394653ad0cdb3d6df6
Chanrich Pisitjing <vjumpkung@gmail.com>
2021-05-18 23:03:25 +0700

    remove cache

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,5 @@
 import { ApolloClient, InMemoryCache, } from "@apollo/client";
 const client = new ApolloClient({
   uri: "https://admin.herocartoontshirt.com/graphql",
-  cache: new InMemoryCache(),
 });
-
 export default client;
